d98a1de535a4e0e3dcf49ec352625071a926b9e1
Gigi M <mmgamao1@gmail.com>
2019-02-09 21:43:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,16 @@
 
   def countdown(count)
-  
+    while count >0
+      puts "#{count} seconds!"
+      count-=1
+    end
+    "0 left. HAPPY NEW YEAR!"
+  end
+
+  def countdown_with_sleep(count)
     while count > 0
-    puts "#{count} SECOND(S)!"
       count-=1
       sleep(1)
     end
-      puts "HAPPY NEW YEAR!"
+      puts "0 left. HAPPY NEW YEAR!"
   end
