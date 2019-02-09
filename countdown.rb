c7279d0250a8c8aeeae4f1aa3511d82f8d2cf170
Gigi M <mmgamao1@gmail.com>
2019-02-09 21:40:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
-
-def countdown
+def countdown_with_sleep
+  sleep(1.second)
 end
+
+
+
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+        countdown_with_sleep
+    seconds_to_midnight -= 1
+ 
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
